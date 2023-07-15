dd294c20cf22f8cca5f8a1a199208d732770fbea
pradip <parkar.p12@gmail.com>
2023-07-15 13:40:02 +0530

    fc commerce

---
+++ app/code/Rest/PraApi/Api/CustomInterface.php
@@ -0,0 +1,14 @@
+<?php
+ 
+namespace Rest\PraApi\Api;
+ 
+interface CustomInterface
+{
+    /**
+     * GET for Post api
+     * @param string $value
+     * @return string
+     */
+ 
+    public function getPost($value);
+}
